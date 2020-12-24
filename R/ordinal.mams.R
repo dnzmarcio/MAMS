e4ed641b08158ecc59c3da4f408ff9caecd675f9
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2020-12-23 19:20:33 -0800

    Add parallelization for tite and ordinal.

--- R/ordinal.mams.R
+++ R/ordinal.mams.R
@@ -1,6 +1,7 @@
 ordinal.mams <- function(prob=c(0.35, 0.4, 0.25), or=2, or0=1.2, K=4, J=2, alpha=0.05, power=0.9,
                          r=1:2, r0=1:2, ushape="obf", lshape="fixed", ufix=NULL, lfix=0, nstart=1,
-                         nstop=NULL, sample.size=TRUE, N=20){
+                         nstop=NULL, sample.size=TRUE, N=20, 
+                         parallel = FALSE, ncores = NULL){
   
   if(sum(prob)!=1){
     stop("The elements of prob must sum to one.")
@@ -20,6 +21,6 @@ ordinal.mams <- function(prob=c(0.35, 0.4, 0.25), or=2, or0=1.2, K=4, J=2, alpha
   
   mams(K=K, J=J, alpha=alpha, power=power, r=r, r0=r0, p=p, p0=p0, delta=NULL, delta0=NULL, sd=NULL,
        ushape=ushape, lshape=lshape, ufix=ufix, lfix=lfix, nstart=nstart, nstop=nstop, sample.size=sample.size,
-       N=N, type="ordinal")
+       N=N, type="ordinal", parallel = parallel, ncores = ncores)
   
 }
--- R/tite.mams.R
+++ R/tite.mams.R
@@ -1,6 +1,7 @@
 tite.mams <- function(hr=1.5, hr0=1.1, K=4, J=2, alpha=0.05, power=0.9, r=1:2, r0=1:2,
                       ushape="obf", lshape="fixed", ufix=NULL, lfix=0, nstart=1,
-                      nstop=NULL, sample.size=TRUE, N=20){
+                      nstop=NULL, sample.size=TRUE, N=20, 
+                      parallel = FALSE, ncores = NULL){
  
   if (hr0 < 1) {
     stop("The uninteresting effect must be 1 or larger.")
@@ -15,6 +16,6 @@ tite.mams <- function(hr=1.5, hr0=1.1, K=4, J=2, alpha=0.05, power=0.9, r=1:2, r
   
   mams(K=K, J=J, alpha=alpha, power=power, r=r, r0=r0, p=p, p0=p0, delta=NULL, delta0=NULL, sd=NULL,
        ushape=ushape, lshape=lshape, ufix=ufix, lfix=lfix, nstart=nstart, nstop=nstop, sample.size=sample.size,
-       N=N, type="tite")
+       N=N, type="tite", parallel = parallel, ncores = ncores)
   
 }
